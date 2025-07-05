d52d256d861ae3e4c2d85f53b0ccac670985f892
Rajasekar M <sekar003scet@gmail.com>
2025-07-05 20:23:19 +0530

    Add: EvenOdd Program (P01)

--- P01_EvenOdd.java
+++ P01_EvenOdd.java
@@ -6,17 +6,17 @@ public class P01_EvenOdd {
         System.out.print("Enter the Number: ");
         int number=scan.nextInt();
 
-        // 1. === Using if-else ===
+        // === 1.Using if-else ===
         // if(number%2==0){
         //     System.out.println("The given number is EVEN");
         // }else{
         // System.out.println("The given number is ODD");
         // }
 
-        // 2. === Using Ternary Operator ===
+        // === 2.Using Ternary Operator ===
         // System.out.println((number%2==0)?"The given number is EVEN":"The given number is ODD");
 
-        // 3. === Using Boolean and if-else ===
+        // === 3.Using Boolean and if-else ===
         // boolean even=false;
         // if(number%2==0)  
         //     even=true;  //using single line body, no need of {} brackets
