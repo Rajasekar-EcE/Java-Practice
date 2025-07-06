10c2562d1ff88310c44f8cde00ff516fb36b9365
Rajasekar M <sekar003scet@gmail.com>
2025-07-06 10:35:16 +0530

    Add: Swap two numbers with and without third variable (P03)

---
+++ P03_SwapTwoNumbers.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class P03_SwapTwoNumbers {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+
+        // === 1.Swap 2 numbers using a third variable ===
+        // System.out.println("=== User Input ===");
+        // System.out.print("Enter the A variable: ");
+        // int A=scan.nextInt();
+        // System.out.print("Enter the B variable: ");
+        // int B=scan.nextInt();
+        // int temp=0; // Third varidable
+        // System.out.println("\n=== After Swapped (With third Variable)===");
+        // temp=A;
+        // A=B;
+        // B=temp;
+        // System.out.println("A variable: "+A);
+        // System.out.println("B variable: "+B);
+
+        // === 2.Swap 2 numbers using without third variable ===
+        // System.out.println("\n=== After Swapped (Without third Variable)===");
+        // A=A+B;
+        // B=A-B;
+        // A=A-B;
+        // System.out.println("A variable: "+A);
+        // System.out.println("B variable: "+B);
+        scan.close();
+    }
+}
