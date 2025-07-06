e63e46474e0881b7cce2b665dc7adbc8d3e67875
Rajasekar M <sekar003scet@gmail.com>
2025-07-06 10:40:08 +0530

    Add: Multiplication table generator (P05)

---
+++ P05_MultiplicationTable.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class P05_MultiplicationTable{
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        // System.out.print("Enter a number to print its multiplication table: ");
+        // int table=scan.nextInt();
+        
+        // === 1.Multiplication Table with range of 10 ===
+        // System.out.println("\nMultiplication table of "+table+": ");
+        // for(int i=1;i<=10;i++){
+        //     System.out.println(i+" * "+table+" = "+(i*table));
+        // }
+
+        // === 2.Multiplication Table With Custom Range ===
+        // System.out.print("Enter your range: ");
+        // int range=scan.nextInt();
+        // System.out.println("\nMultiplication table of "+table+": ");
+        // for(int i=1;i<=range;i++){
+        //     System.out.println(i+" * "+table+" = "+(i*table));
+        // }
+        scan.close();
+    }
+}
